979e0dd535cb84b2700540b025e91111801d38e2
CesarGabriel <133375639+cesar1227B@users.noreply.github.com>
2024-10-04 15:54:51 -0300

    Add files via upload

---
+++ flashcard-aula_4/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Cultura POP',
+    'Qual foi a primeira música a viralizar do cantor JÃO?',
+    'Imaturo, 2018'
+)
+
+criaCartao(
+    'Cultura POP',
+    'De que música do cantor JÃO pertence esse trecho "Todo verão termina, eu amo despedida e se eu te amei, eu peço perdão..."?',
+    'Santo, 2021'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Qual o ano de nascimento do cantor JÃO?',
+    'Ele nasceu em 1994'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Em qual almbúm do JÃO estão as músicas "Santo" e "Idiota?"',
+    'Estão no albúm "Pirata"'
+)
+
+criaCartao(
+    'Cultura POP',
+    'A que música do JÃO pertence esse trecho "Eu não quero mais beber assim, na esperança de te ter pra mim..."?',
+    'Pertence a música "álcool"'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Em que ano o cantor JÃO fez sua priemira turne?',
+    'Sua primeira turnê foi em 2023'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Qual o nome do namorado do cantor JÃO?',
+    'O nome dele é Pedro'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Quem foi o cantor que, junto com o JÃO, gravou no ano de 2024, a música "mal"?',
+    'Foi o cantor e compositor Gustavo Mioto'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Em que ano o cantor JÃO assumiu públicamente seu relacionamento com seu namorado Pedro?',
+    'Foi no ano de 2024 num show em São Paulo, no Allianz Parque'
+)
